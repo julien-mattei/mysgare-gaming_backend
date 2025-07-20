e93a4aa7d435b2db9f11ab11c02d295b9725957f
Julien Mattei <j.mattei@me.com>
2025-07-20 11:55:25 +0200

    add seeding files to seed the tables

---
+++ app/seeding/seeding.cover.js
@@ -0,0 +1,27 @@
+import 'dotenv/config';
+import { sequelize } from '../models/db.client.js';
+import { Cover, Game } from '../models/associations.js';
+import { games } from '../datas/game.datas.js';
+
+const image_sizes = ['thumb', 'cover_big', '720p', '1080p', 'original'];
+
+for(const game of games){
+    const game_title = await Game.findOne({
+        where : {title: game.titre}
+    })
+    for(const size of image_sizes){
+        await Cover.findOrCreate({
+            where: {name: `${game.titre}_${size}`},
+            defaults: {
+                url: `https://images.igdb.com/igdb/image/upload/t_${size}/${game.image.image_id}.webp`,
+                size: size,
+                game_id: game_title.id,
+            },
+        ignoreDuplicates: true
+        })
+    }   
+};
+
+console.log("seeding done");
+
+await sequelize.close()
--- app/seeding/seeding.genre.js
+++ app/seeding/seeding.genre.js
@@ -1,48 +1,27 @@
 import 'dotenv/config';
 import { sequelize } from '../models/db.client.js';
-import {Game, Genre, Cover } from '../models/associations.js';
+import { Genre, Game } from '../models/associations.js';
 import { games } from '../datas/game.datas.js';
+
 
 let genresList = [];
 const genres = [];
 
-const image_sizes = ['thumb', 'cover_big', '720p', '1080p', 'original']
-
-// Get all genre with no duplicate then seed table genre
+// Get all genre with no duplicate then seed table genre with findorcreate method
 for(const game of games){
     for(const genre of game.genres){
         genres.push(genre)
     }
-}
+};
 genresList = new Set(genres)
 
 for(const genre of genresList){
-    await Genre.create({
-        name : genre
-    })
-}
-
-// Seed table game
-for(const game of games){
-    await Game.create({
-        title: game.titre,
-        year: game.annee
-    })
-}
-
-for(const game of games){
-    const game_title = await Game.findOne({
-        where : {title: game.titre}
-    })
-    for(const size of image_sizes){
-        await Cover.create({
-        name: `${game.titre}_${size}`,
-        url: `https://images.igdb.com/igdb/image/upload/t_${size}/${game.image.image_id}`,
-        size: size,
-        game_id: game_title.id
+    await Genre.findOrCreate(
+        {
+            where: { name: genre },
+            ignoreDuplicates: true
         })
-    }   
-}
+};
 
 // Get game and genre by title and name in order to seed table game_has_genre
 for(const gameData of games){
@@ -56,7 +35,7 @@ for(const gameData of games){
     })
 
     await gameInstance.addGenre(genreInstance)
-}
+};
 
 console.log("seeding done");
 
