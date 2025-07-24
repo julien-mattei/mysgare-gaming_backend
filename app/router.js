094078396f055fec6fc73bb6aed7e134208f4ec3
Julien Mattei <j.mattei@me.com>
2025-07-24 14:21:48 +0200

    create files for services and controller for boss

--- app/router.js
+++ app/router.js
@@ -1,6 +1,7 @@
 import { Router } from "express";
 import { gamesController } from "./controllers/games.controller.js";
 
+
 export const router = new Router();
 
 router.get("/api/games", gamesController.getAllGames)
@@ -9,3 +10,5 @@ router.get("/api/games/random", gamesController.getRandomGames)
 router.get("/api/games/toVoted", gamesController.getGamesToVoted)
 router.get("/api/games/isCurrent", gamesController.getCurrentGame)
 router.get("/api/games/:id", gamesController.getOneGame)
+
+
---
+++ app/services/boss.services.js
@@ -0,0 +1,14 @@
+import { sequelize } from "../models/db.client.js";
+import { Game, Boss } from "../models/associations.js"
+
+export function addBoss(){
+
+}
+
+export function updateBoss(){
+    
+}
+
+export function deleteBoss(){
+    
+}
